7c8fa26a1f06db63f85a4365aa0136d527412dc3
Christo4ffff <67094013+Christo4ffff@users.noreply.github.com>
2020-06-29 08:35:10 +0200

    Create NoMoreIcecreamException.java

---
+++ NoMoreIcecreamException.java
@@ -0,0 +1,9 @@
+  
+package be.vdab.IceCreamShop.application;
+
+public class NoMoreIceCreamException extends Exception{
+
+    public NoMoreIceCreamException(String message) {
+        super(message);
+    }
+}
